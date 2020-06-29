7642d3b07c4f542bf1372af1f318f1c656659cd4
Marc O'Morain <marc@circleci.com>
2020-06-29 17:43:22 +0100

    new hostname

--- fake_webhook.sh
+++ fake_webhook.sh
@@ -4,4 +4,4 @@ curl -X POST \
   -H "Webhook-Url: $CIRCLE_BUILD_URL" \
   -H "Webhook-Username: $CIRCLE_USERNAME" \
   -H "Webhook-Project: $CIRCLE_PROJECT_REPONAME" \
-  "https://0c3c11df2ae2.ngrok.io/circleci/fake-webhook"
+  "https://ecosystem.eu.ngrok.io/circleci/fake-webhook"
