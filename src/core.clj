9c5d5998fc9f5403f94db6172fc3e11b3cdd23ef
Marc O'Morain <marc@circleci.com>
2021-09-06 16:27:34 +0100

    print timestamps

--- src/core.clj
+++ src/core.clj
@@ -1,8 +1,23 @@
 (ns core
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:import
+   java.time.Instant
+   java.io.File))
+
+
+(defn modified [^File file]
+
+  (Instant/ofEpochMilli
+   (.lastModified file)))
 
 (defn -main []
   (println "Hello, world")
-  (let [f (io/file "test.txt")]
-    (when-not (.exists f)
-      (spit f "test content\n"))))
+  (let [cached (io/file "test.txt")]
+    (when-not (.exists cached)
+      (spit cached "test content\n"))
+
+
+
+    (printf "now:         %s \n" (Instant/now))
+    (printf "cached file: %s \n" (modified cached))
+    (printf "source file: %s \n" (modified (io/file "src/core.clj")))))
